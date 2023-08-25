470b7ca995e0841e3700ed0f5d8ee04f9ce1e865
mmgmolotov <mmgmolotov@gmail.com>
2023-08-25 14:39:27 -0700

    mouad u r blocked

--- sh_func.c
+++ sh_func.c
@@ -2,16 +2,18 @@
 
 /**
  * main - Entry point
- *
+ * @argc:arg
+ * @argv:argv
  * Description - let's try to create a little shell
  * Return: void
 */
-int main(void)
+int main(int argc, char  **argv)
 {
 	ssize_t r_bts;
 	char *in_buf;
 	size_t buf_size;
 
+	void(argc);
 	r_bts = 0;
 	in_buf = NULL;
 	buf_size = 0;
@@ -28,13 +30,17 @@ int main(void)
 		{
 			_EOF(in_buf);
 		}
-		else if (r_bts == 1 || strspn(in_buf, " \t\n") == (size_t)r_bts || strspn(in_buf, " \t\n") == (((size_t)r_bts - 1)))
+		else if (strcmp(in_buf, "exit\n") == 0)
+		{
+			_EOF(in_buf);
+		}
+		else if (r_bts == 1 || strspn(in_buf, " \t\n") == (size_t)r_bts)
 		{
 			continue;
 		}
 		else
 		{
-			proc_input(in_buf, NULL);
+			proc_input(in_buf, &argv[0]);
 		}
 	}
 	free(in_buf);
